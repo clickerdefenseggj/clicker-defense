8dc0a409ca75628513d6868ef106b9417c959bc2
Blake Triana <blake.triana@gmail.com>
2016-01-28 01:30:09 -0800

    UnitTemplate and GameData

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class GameData
+{
+    public static Dictionary<string, UnitTemplate> unitTemplates =
+        new Dictionary<string, UnitTemplate>()
+        {
+            { "bandit", new UnitTemplate() { scoreValue = 5, goldValue = 10, maxHealth = 200  } },
+            { "archer", new UnitTemplate() { scoreValue = 7, goldValue = 15, maxHealth = 170  } },
+            { "giant", new UnitTemplate() { scoreValue = 12, goldValue = 40, maxHealth = 650  } }
+        };
+}
---
+++ Assets/Scripts/UnitTemplate.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitTemplate
+{
+    public int scoreValue;
+    public int goldValue;
+    public int maxHealth;
+}
