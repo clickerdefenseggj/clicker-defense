0b8b1a12cf6b9595098acf0f3a055c788c2ef130
Jovanni Cutigni <thejacx@gmail.com>
2016-01-29 23:11:13 -0800

    Add incomplete store stuff

---
+++ Assets/Scripts/StoreCategoryElement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StoreCategoryElement : MonoBehaviour
+{
+    public Text TitleText;
+    public RectTransform ContentTransform;
+
+    public string CategoryName;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
